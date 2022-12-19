7e00dffe1cde7e2e1ab64b8dc9662af57dcd523f
Jooyeon-Kimm <jooooyeonkim@gmail.com>
2022-12-20 00:09:56 +0900

    추천(recommend)/구글맵(webview)
    추천은 recommend
    구글맵은 webview

---
+++ MyApplication/googlemap/src/main/java/com/example/googlemap/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.googlemap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val marker = LatLng(35.241615, 128.695587)
+        mMap.addMarker(MarkerOptions().position(marker).title("마커 제목"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
+    }
+}
---
+++ MyApplication/googlemaps/src/main/java/com/example/googlemaps/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.googlemaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Transformations.map
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
